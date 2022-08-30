6cbed69034814ddba941d3b6520eb2d1f2cf3c32
Max Blancarte <blancarte@gmail.com>
2022-08-29 23:31:04 -0700

    feat: added images

--- src/data/entrenamientoData.ts
+++ src/data/entrenamientoData.ts
@@ -1,22 +1,30 @@
 export const entrenamiento = [
-  {
-    id: 1,
-    contents:
-      "Conocerás la fórmula para iniciar y crecer en el mundo digital. Verás las nuevas tendencia del mercado.",
-  },
-  {
-    id: 2,
-    contents:
-      "Te enseñaré cómo captar clientes utilizando tus redes sociales, así no tenga seguidores. Además, te revelaré el paso a paso para que te lleguen pacientes de diferentes países.",
-  },
-  {
-    id: 3,
-    contents:
-      "Te revelaré la fórmula que yo usé profesionalizar mi negocio, de esta forma podrás establecer tu marca de una manera sencilla y segura. Generarás autoridad en el mercado.",
-  },
-  {
-    id: 4,
-    contents:
-      "Descubrirás cómo trascender tu negocio y aumentar tu facturación sin necesidad de salir de tu consultorio u oficina.",
-  },
+	{
+		id: 1,
+		contents:
+			'Conocerás la fórmula para iniciar y crecer en el mundo digital. Verás las nuevas tendencia del mercado.',
+		img: 'claseRibbon1gray.png',
+		alt: 'Clase 1',
+	},
+	{
+		id: 2,
+		contents:
+			'Te enseñaré cómo captar clientes utilizando tus redes sociales, así no tenga seguidores. Además, te revelaré el paso a paso para que te lleguen pacientes de diferentes países.',
+		img: 'claseRibbon2gray.png',
+		alt: 'Clase 2',
+	},
+	{
+		id: 3,
+		contents:
+			'Te revelaré la fórmula que yo usé profesionalizar mi negocio, de esta forma podrás establecer tu marca de una manera sencilla y segura. Generarás autoridad en el mercado.',
+		img: 'claseRibbon3gray.png',
+		alt: 'Clase 3',
+	},
+	{
+		id: 4,
+		contents:
+			'Descubrirás cómo trascender tu negocio y aumentar tu facturación sin necesidad de salir de tu consultorio u oficina.',
+		img: 'claseRibbon4gray.png',
+		alt: 'Clase 4',
+	},
 ];
