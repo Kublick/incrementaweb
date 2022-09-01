1b360844495fa9df6ee56752afb0c83f705eed21
Max Blancarte <blancarte@gmail.com>
2022-08-31 18:15:57 -0700

    feat: refactor counter

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,40 +1,41 @@
-import { useCountdown } from '../hooks/useCountdown';
-import DateTimeDisplay from './DateTimeDisplay';
+import { useCountdown } from "../hooks/useCountdown";
+import DateTimeDisplay from "./DateTimeDisplay";
 
 const ExpiredNotice = () => {
-	return (
-		<div className="">
-			<p className="text-red-400 text-2xl">El tiempo de registro ya expiro</p>
-		</div>
-	);
+  return (
+    <div className="">
+      <p className="text-red-400 text-2xl">El tiempo de registro ya expiro</p>
+    </div>
+  );
 };
 
-const ShowCounter = ({ days, hours, minutes, seconds }: any) => {
-	return (
-		<div className="flex gap-4 text-center">
-			<DateTimeDisplay value={days} type={'Dias'} />
-			<DateTimeDisplay value={hours} type={'Horas'} />
-			<DateTimeDisplay value={minutes} type={'Mins'} />
-			<DateTimeDisplay value={seconds} type={'Seg'} />
-		</div>
-	);
+const ShowCounter = ({ days, hours, minutes, seconds, textColor }: any) => {
+  return (
+    <div className={`flex gap-4 text-center`}>
+      <DateTimeDisplay value={days} type={"Dias"} textColor={textColor} />
+      <DateTimeDisplay value={hours} type={"Horas"} textColor={textColor} />
+      <DateTimeDisplay value={minutes} type={"Mins"} textColor={textColor} />
+      <DateTimeDisplay value={seconds} type={"Seg"} textColor={textColor} />
+    </div>
+  );
 };
 
-const CountdownTimer = ({ targetDate }: any) => {
-	const [days, hours, minutes, seconds] = useCountdown(targetDate);
+const CountdownTimer = ({ targetDate, textColor }: any) => {
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
 
-	if (days + hours + minutes + seconds <= 0) {
-		return <ExpiredNotice />;
-	} else {
-		return (
-			<ShowCounter
-				days={days}
-				hours={hours}
-				minutes={minutes}
-				seconds={seconds}
-			/>
-		);
-	}
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice />;
+  } else {
+    return (
+      <ShowCounter
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+        textColor={textColor}
+      />
+    );
+  }
 };
 
 export default CountdownTimer;
--- src/components/DateTimeDisplay.tsx
+++ src/components/DateTimeDisplay.tsx
@@ -1,10 +1,10 @@
-const DateTimeDisplay = ({ value, type }: any) => {
-	return (
-		<div>
-			<p className="font-semibold text-xl md:text-3xl">{value}</p>
-			<span className="text-sm">{type}</span>
-		</div>
-	);
+const DateTimeDisplay = ({ value, type, textColor }: any) => {
+  return (
+    <div className={`text-${textColor}`}>
+      <p className="font-semibold text-xl md:text-3xl w-12 ">{value}</p>
+      <span className="text-sm">{type}</span>
+    </div>
+  );
 };
 
 export default DateTimeDisplay;
