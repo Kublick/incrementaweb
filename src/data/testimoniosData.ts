64d12af95a41e00eef8a9dda22b5413ca63794b6
Max Blancarte <blancarte@gmail.com>
2022-08-31 22:57:21 -0700

    feat: PLC1 Completed

--- src/data/testimoniosData.ts
+++ src/data/testimoniosData.ts
@@ -1,26 +1,26 @@
 export const testimoniosData = [
-	{
-		id: 1,
-		titulo: 'Psicóloga',
-		nombre: 'Carolina Benitez',
-		contenido:
-			'Logre vender mi primer paquete de consultas internacional desde mi país Honduras, ahora tengo  paciente en Peru para mi consulta Online.',
-		img: 'carolina_benitez.webp',
-	},
-	{
-		id: 2,
-		titulo: 'Psicóloga',
-		nombre: 'Elba Garcia',
-		contenido:
-			'Logre ganar más de $1,000 dólares con mis primeras ventas de consultas, con lo que aprendi en el programa Incrementa Tu Consulta.',
-		img: 'elba_garcia.webp',
-	},
-	{
-		id: 3,
-		titulo: 'Psicóloga',
-		nombre: 'Vidalia Herrera',
-		contenido:
-			'Logre aprender a conseguir pacientes a través de redes sociales y vender mis primeras consultas tanto presenciales como online con lo que aprendi',
-		img: 'vidalia__herrera.webp',
-	},
+  {
+    id: 1,
+    titulo: "Psicóloga",
+    nombre: "Carolina Benitez",
+    contenido:
+      "Logre vender mi primer paquete de consultas internacional desde mi país Honduras, ahora tengo  paciente en Peru para mi consulta Online.",
+    img: "carolina_benitez.webp",
+  },
+  {
+    id: 2,
+    titulo: "Psicóloga",
+    nombre: "Elba Garcia",
+    contenido:
+      "Logre ganar más de $1,000 dólares con mis primeras ventas de consultas, con lo que aprendi en el programa Incrementa Tu Consulta.",
+    img: "elba_garcia.webp",
+  },
+  {
+    id: 3,
+    titulo: "Psicóloga",
+    nombre: "Vidalia Herrera",
+    contenido:
+      "Logre aprender a conseguir pacientes a través de redes sociales y vender mis primeras consultas tanto presenciales como online con lo que aprendi",
+    img: "vidalia__herrera.webp",
+  },
 ];
