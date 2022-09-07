d987823ab80bd8ed0a8a3e762742ae53640abf94
Max Blancarte <blancarte@gmail.com>
2022-09-07 13:10:41 -0700

    fix: optimizations

---
+++ src/hooks/getRegistro.ts
@@ -0,0 +1,17 @@
+export const getRegistro = async () => {
+	const id = '7YXamvsw6hSppoy-s2ggg';
+	const aws = 'https://6u4nv7easg.execute-api.us-west-1.amazonaws.com/url/';
+
+	try {
+		const response = await fetch(`${aws}/${id}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const data = await response.json();
+		return data.body.link;
+	} catch (error) {
+		console.log(error);
+	}
+};
