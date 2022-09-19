2f0b1cf1d5aa32e9009fbc57a1c2d07e9abfa8ff
Max Blancarte <blancarte@gmail.com>
2022-09-18 17:42:36 -0700

    feat: update to leadmagnet

--- src/components/CTAReplay.tsx
+++ src/components/CTAReplay.tsx
@@ -1,87 +1,87 @@
-import { Transition, Dialog } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { Transition, Dialog } from '@headlessui/react';
+import { Fragment, useState } from 'react';
 
-import ReplayFormulario from "./ReplayFormulario";
+import ReplayFormulario from './ReplayFormulario';
 
-const CTAReplay = () => {
-  let [isOpen, setIsOpen] = useState(false);
+function CTAReplay() {
+	let [isOpen, setIsOpen] = useState(false);
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+	function closeModal() {
+		setIsOpen(false);
+	}
 
-  function openModal() {
-    setIsOpen(true);
-  }
+	function openModal() {
+		setIsOpen(true);
+	}
 
-  return (
-    <>
-      <div className="mt-8 flex items-center flex-col">
-        <button
-          className="px-5 py-2.5 relative rounded group overflow-hidden bg-pink-50 text-secondary inline-block text-xl md:text-2xl font-semibold border-secondary border-2"
-          onClick={openModal}
-        >
-          <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-secondary group-hover:h-full opacity-90"></span>
-          <span className="relative group-hover:text-white">
-            Confirma tu asistencia aquí
-          </span>
-        </button>
-      </div>
+	return (
+		<>
+			<div className="mt-8 flex items-center flex-col">
+				<button
+					className="px-5 py-2.5 relative rounded group overflow-hidden bg-pink-50 text-secondary inline-block text-xl md:text-2xl font-semibold border-secondary border-2"
+					onClick={openModal}
+				>
+					<span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-secondary group-hover:h-full opacity-90"></span>
+					<span className="relative group-hover:text-white">
+						Confirma tu asistencia aquí
+					</span>
+				</button>
+			</div>
 
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog className="relative z-10" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-50" />
-          </Transition.Child>
+			<Transition appear show={isOpen} as={Fragment}>
+				<Dialog className="relative z-10" onClose={closeModal}>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div className="fixed inset-0 bg-black bg-opacity-50" />
+					</Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-secondary p-6 text-left align-middle shadow-xl transition-all ">
-                  <div className="flex">
-                    <button onClick={closeModal} className="self-end">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6 text-white my-2"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                      </svg>
-                    </button>
-                  </div>
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex min-h-full items-center justify-center p-4 text-center">
+							<Transition.Child
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
+							>
+								<Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-secondary p-6 text-left align-middle shadow-xl transition-all ">
+									<div className="flex">
+										<button onClick={closeModal} className="self-end">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												strokeWidth={1.5}
+												stroke="currentColor"
+												className="w-6 h-6 text-white my-2"
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+												/>
+											</svg>
+										</button>
+									</div>
 
-                  <ReplayFormulario />
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-    </>
-  );
-};
+									<ReplayFormulario />
+								</Dialog.Panel>
+							</Transition.Child>
+						</div>
+					</div>
+				</Dialog>
+			</Transition>
+		</>
+	);
+}
 
 export default CTAReplay;
