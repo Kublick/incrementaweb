f9f2e494e42be3176cdaa2ddeb7bfa4ccf8bdf40
Max Blancarte <blancarte@gmail.com>
2022-10-08 12:33:30 -0700

    feat: Venta Bonos Programa

---
+++ src/components/venta/ventaData.ts
@@ -0,0 +1,108 @@
+export const puntos = [
+	{
+		id: 1,
+		image: '/venta/imagen1.png',
+		description:
+			'El método completo para empezar a atraer pacientes a tu consultorio (sin importar tu especialización)',
+	},
+	{
+		id: 2,
+		image: '/venta/imagen2.png',
+		description: 'Vencerás el miedo a la tecnología y a las redes sociales',
+	},
+	{
+		id: 3,
+		image: '/venta/imagen3.png',
+		description:
+			'Sabrás cómo aumentar el valor de tus consultas y cobrar lo que realmente te mereces',
+	},
+	{
+		id: 4,
+		image: '/venta/imagen4.png',
+		description:
+			'Abrirás tus fronteras y conocerás cómo vender tus servicios en el exterior (sin necesidad de moverte de tu oficina)',
+	},
+	{
+		id: 5,
+		image: '/venta/imagen5.png',
+		description: 'Ganarás confianza y seguridad en ti',
+	},
+	{
+		id: 6,
+		image: '/venta/imagen6.png',
+		description:
+			'Tu negocio empezará a crecer y hasta tendrás que delegar algunas tareas que te quitan tiempo',
+	},
+	{
+		id: 7,
+		image: '/venta/imagen7.png',
+		description:
+			'Te posicionarás como autoridad en tu área y destacarás de tu competencia',
+	},
+	{
+		id: 8,
+		image: '/venta/imagen8.png',
+		description:
+			'Tendrás una comunidad que te ayudará en todo momento y empujará a tener más resultados',
+	},
+];
+
+export const programaData = [
+	{
+		id: 1,
+		image: '/venta/programa1.png',
+		title: 'Programa intensivo de 8 semanas.',
+		description:
+			'Es 100% online y aprenderás la estrategia completa para atraer a pacientes a tu consultorio todos los días,sin importar el área en que seas especialista.Mejorarás tu número de consultas semanales y multiplicarás tus ingresos.',
+		value: 'Valor 997 USD.',
+	},
+	{
+		id: 2,
+		image: '/venta/programa2.png',
+		title: '8 mentorías grupales en vivo.',
+		description:
+			'En equipo todo es mejor. Semana a semana nos reuniremos para hacer seguimiento a tus avances. Las dudas las aclararemos en vivo y te ayudaremos a alcanzar tus metas. No estarás solo en este proceso.',
+		value: 'Valor 697 USD.',
+	},
+	{
+		id: 3,
+		image: '/venta/programa3.png',
+		title: '8 módulos de trabajo con lecciones de video en HD.',
+		description:
+			'Tendrás acceso a todo el programa para que puedas consumirlo de forma ordenada y enfocada. Podrás ir a tu ritmo, elige tus propios horarios para ver las lecciones pregrabadas y tomar apuntes. Conéctate desde la comunidad de tu casa, oficina o  consultorio en tu computador o celular',
+		value: 'Valor XXX USD.',
+	},
+
+	{
+		id: 4,
+		image: '/venta/programa4.png',
+		title: 'El calendario de un profesional TOP .',
+		description:
+			'Te entregaremos un plan de estudio en PDF para que sepas exactamente en dónde debes de enfocarte cada semana, para que avances con pasos firmes hacia tus objetivos.',
+		value: 'Valor 47 USD.',
+	},
+	{
+		id: 5,
+		image: '/venta/programa5.png',
+		title: 'Acelerador de resultados.',
+		description:
+			'Para lograr resultados cada semana debes tener metas específicas, alcanzables y medibles. Aquí conocerás cómo multiplicar tus resultados para que nada quede en teoría, cada lección te llevará a la acción con ejercicios detallados para que apliques en tu negocio y comiences a ver los frutos de tu esfuerzo.',
+		value: 'Valor XXX USD.',
+	},
+	{
+		id: 6,
+		image: '/venta/programa6.png',
+		title: 'Ticket VIP por un año.',
+		description:
+			'Tendrás acceso por los siguientes 12 meses a la plataforma y a todo el contenido, para que así puedas ver y repetir las lecciones las veces que tú prefieras. Además, podrás ser parte de todas las actualizaciones e información relevante que te ayudará a potenciar tu negocio.',
+		value: 'Valor 497 USD.',
+	},
+	{
+		id: 7,
+		image: '/venta/programa7.png',
+		title: 'Membresía en comunidad internacional.',
+		description:
+			'Es 100% online y aprenderás la estrategia completa para atraer a pacientes a tu consultorio todos los días, sin importar el área en que seas especialista. Mejorarás tu número de consultas semanales y multiplicarás tus ingresos.',
+		value: 'Invaluable',
+	},
+];
