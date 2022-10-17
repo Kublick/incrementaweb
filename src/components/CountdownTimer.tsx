cfdbf518a8b1f9bc8d2ad8ecc1ebe23484cd4ce8
Max Blancarte <blancarte@gmail.com>
2022-10-17 11:05:26 -0700

    fix: adjust end error

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,43 +1,43 @@
-import { useCountdown } from '../hooks/useCountdown';
-import DateTimeDisplay from './DateTimeDisplay';
+import { useCountdown } from "../hooks/useCountdown";
+import DateTimeDisplay from "./DateTimeDisplay";
 
 const targetDate = new Date().getTime() + 15 * 60 * 1000;
 
 const ExpiredNotice = () => {
-	return (
-		<div className="">
-			<p className="text-red-400 text-2xl">El tiempo de registro ya expiro</p>
-		</div>
-	);
+  return (
+    <div className="">
+      <p className="text-white text-2xl">00 : 00 : 00 : 00</p>
+    </div>
+  );
 };
 
 const ShowCounter = ({ days, hours, minutes, seconds, textColor }: any) => {
-	return (
-		<div className={`flex gap-4 text-center`}>
-			<DateTimeDisplay value={days} type={'Dias'} textColor={textColor} />
-			<DateTimeDisplay value={hours} type={'Horas'} textColor={textColor} />
-			<DateTimeDisplay value={minutes} type={'Mins'} textColor={textColor} />
-			<DateTimeDisplay value={seconds} type={'Seg'} textColor={textColor} />
-		</div>
-	);
+  return (
+    <div className={`flex gap-4 text-center`}>
+      <DateTimeDisplay value={days} type={"Dias"} textColor={textColor} />
+      <DateTimeDisplay value={hours} type={"Horas"} textColor={textColor} />
+      <DateTimeDisplay value={minutes} type={"Mins"} textColor={textColor} />
+      <DateTimeDisplay value={seconds} type={"Seg"} textColor={textColor} />
+    </div>
+  );
 };
 
 export function CountdownTimer({ targetDate, textColor }: any) {
-	const [days, hours, minutes, seconds] = useCountdown(targetDate);
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
 
-	if (days + hours + minutes + seconds <= 0) {
-		return <ExpiredNotice />;
-	} else {
-		return (
-			<ShowCounter
-				days={days}
-				hours={hours}
-				minutes={minutes}
-				seconds={seconds}
-				textColor={textColor}
-			/>
-		);
-	}
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice />;
+  } else {
+    return (
+      <ShowCounter
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+        textColor={textColor}
+      />
+    );
+  }
 }
 
 export default CountdownTimer;
