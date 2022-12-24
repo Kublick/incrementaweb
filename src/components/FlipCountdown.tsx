4d55e261bafda8e7fbc3929ba9198ffc9f712f1f
Max Blancarte <blancarte@gmail.com>
2022-12-24 15:31:48 -0800

    feat: update hero section

--- src/components/FlipCountdown.tsx
+++ src/components/FlipCountdown.tsx
@@ -1,27 +1,27 @@
-import FlipCountdown from '@rumess/react-flip-countdown';
-import useWindowDimensions from './hooks/useWindowDimensions';
+import FlipCountdown from "@rumess/react-flip-countdown";
+import useWindowDimensions from "./hooks/useWindowDimensions";
 
-export const FlipCD = ({ date = 'October 21 2022 00:12' }) => {
-	const { width } = useWindowDimensions();
+export const FlipCD = ({ date = "February 02 2023 00:00" }) => {
+  const { width } = useWindowDimensions();
 
-	let textSize = 'medium';
+  let textSize = "medium";
 
-	if (width! < 530) {
-		textSize = 'small';
-	}
+  if (width! < 530) {
+    textSize = "small";
+  }
 
-	return (
-		<FlipCountdown
-			titlePosition="bottom"
-			size={textSize}
-			hideYear
-			hideMonth
-			endAtZero
-			dayTitle="Dias"
-			hourTitle="Hrs"
-			minuteTitle="Min"
-			secondTitle="Seg"
-			endAt={date} // Date/Time
-		/>
-	);
+  return (
+    <FlipCountdown
+      titlePosition="bottom"
+      size={textSize}
+      hideYear
+      hideMonth
+      endAtZero
+      dayTitle="Dias"
+      hourTitle="Hrs"
+      minuteTitle="Min"
+      secondTitle="Seg"
+      endAt={date} // Date/Time
+    />
+  );
 };
