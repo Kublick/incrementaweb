ff3935d0195344b814b1c522b175619bb2373caa
Max Blancarte <blancarte@gmail.com>
2022-12-24 16:13:11 -0800

    feat: update to entrenamiento

--- src/data/entrenamientoData.ts
+++ src/data/entrenamientoData.ts
@@ -1,42 +1,34 @@
 export const entrenamiento = [
-	{
-		id: 1,
-		contents:
-			'Conocerás la fórmula para iniciar y crecer en el mundo digital. Verás las nuevas tendencia del mercado.',
-		img: 'claseRibbon1gray.png',
-		alt: 'Clase 1',
-		activeImg: 'berenice_Ribboncolor1.png',
-		openDate: Number(new Date('17 October 2022 16:00')),
-		url: 'plc1',
-	},
-	{
-		id: 2,
-		contents:
-			'Te enseñaré cómo captar clientes utilizando tus redes sociales, así no tenga seguidores. Además, te revelaré el paso a paso para que te lleguen pacientes de diferentes países.',
-		img: 'claseRibbon2gray.png',
-		alt: 'Clase 2',
-		activeImg: 'berenice_Ribboncolor2.png',
-		openDate: Number(new Date('18 october 2022 16:30')),
-		url: 'plc2',
-	},
-	{
-		id: 3,
-		contents:
-			'Te revelaré la fórmula que yo usé para profesionalizar mi negocio, de esta forma podrás establecer tu marca de una manera sencilla y segura. Generarás autoridad en el mercado.',
-		img: 'claseRibbon3gray.png',
-		alt: 'Clase 3',
-		activeImg: 'berenice_Ribboncolor3.png',
-		openDate: Number(new Date('19 october 2022 16:30')),
-		url: 'plc3',
-	},
-	{
-		id: 4,
-		contents:
-			'Descubrirás cómo trascender tu negocio y aumentar tu facturación sin necesidad de salir de tu consultorio u oficina.',
-		img: 'claseRibbon4gray.png',
-		alt: 'Clase 4',
-		activeImg: 'berenice_Ribboncolor4.png',
-		openDate: Number(new Date('20 october 2022 16:30')),
-		url: 'plc4',
-	},
+  {
+    id: 1,
+    title: "Clase #1",
+    contents:
+      "Conocerás la fórmula para iniciar y crecer en el mundo digital. Verás las nuevas tendencia del mercado.",
+    img: "Clase_1.webp",
+    alt: "Clase 1",
+  },
+  {
+    id: 2,
+    title: "Clase #2",
+    contents:
+      "Te enseñaré cómo captar clientes utilizando tus redes sociales, así no tenga seguidores. Además, te revelaré el paso a paso para que te lleguen pacientes de diferentes países.",
+    img: "Clase_2.webp",
+    alt: "Clase 2",
+  },
+  {
+    id: 3,
+    title: "Clase #3",
+    contents:
+      "Te revelaré la fórmula que yo usé para profesionalizar mi negocio, de esta forma podrás establecer tu marca de una manera sencilla y segura. Generarás autoridad en el mercado.",
+    img: "Clase_3.webp",
+    alt: "Clase 3",
+  },
+  {
+    id: 4,
+    title: "Clase #4",
+    contents:
+      "Descubrirás cómo trascender tu negocio y aumentar tu facturación sin necesidad de salir de tu consultorio u oficina.",
+    img: "Clase_4.webp",
+    alt: "Clase 4",
+  },
 ];
