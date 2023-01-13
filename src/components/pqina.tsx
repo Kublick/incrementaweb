b29582dc670dce981078d9a4a8796a159360b24c
Max Blancarte <blancarte@gmail.com>
2023-01-13 14:10:25 -0800

    fix: update contents

--- src/components/pqina.tsx
+++ src/components/pqina.tsx
@@ -1,11 +1,11 @@
 import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
 import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 import "../utils/flip.css";
-export const ChinneseCountDown = () => {
+export const ChinneseCountDown = ({ date = "2023-01-25 16:40" }) => {
   return (
     <FlipClockCountdown
       className="flip-clock"
-      to={new Date("2023-01-25 16:40")}
+      to={new Date(date)}
       digitBlockStyle={{ fontSize: 36, fontFamily: "Coolvetica" }}
       showLabels={false}
       showSeparators={true}
