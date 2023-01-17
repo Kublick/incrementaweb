c26aa657226cccd8a4bddfbbea7a7d4e88e652a8
Max Blancarte <blancarte@gmail.com>
2023-01-17 13:45:38 -0800

    fix: update func

--- src/hooks/getLead.ts
+++ src/hooks/getLead.ts
@@ -1,17 +1,17 @@
 export const getLead = async () => {
-	const id = '7YXamvsw6hSppoy-s2glm';
-	const aws = 'https://6u4nv7easg.execute-api.us-west-1.amazonaws.com/url/';
+  const id = "7YXamvsw6hSppoy-s2glm";
+  const aws = "https://6u4nv7easg.execute-api.us-west-1.amazonaws.com/url/";
 
-	try {
-		const response = await fetch(`${aws}/${id}`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-		const data = await response.json();
-		return data.body.link;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await fetch(`${aws}/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data.body.link;
+  } catch (error) {
+    console.log(error);
+  }
 };
