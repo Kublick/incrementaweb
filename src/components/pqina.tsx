a600e27fa60a6b51243df6b740a2b208851a06ad
Max Blancarte <blancarte@gmail.com>
2023-01-23 17:10:43 -0800

    feat: update sale site

--- src/components/pqina.tsx
+++ src/components/pqina.tsx
@@ -1,10 +1,18 @@
 import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
 import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 import "../utils/flip.css";
-export const ChinneseCountDown = ({ date = "2023-01-25 16:40" }) => {
+
+type Props = {
+  date?: string;
+  color: boolean;
+};
+export const ChinneseCountDown = ({
+  date = "2023-01-25 16:40",
+  color,
+}: Props) => {
   return (
     <FlipClockCountdown
-      className="flip-clock"
+      className={color ? "flip-clock" : "flip-clock-black"}
       to={new Date(date)}
       digitBlockStyle={{ fontSize: 36, fontFamily: "Coolvetica" }}
       showLabels={false}
