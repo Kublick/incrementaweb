c572517da429a0f479fcd3ff0fa711c8024806d2
Max Blancarte <blancarte@gmail.com>
2023-09-03 14:11:12 -0700

    fix: update forms

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,3 +7,6 @@ export const salesEndDate = "2023-09-21 23:00";
 export const endEspecialEventDate = "2023-09-10";
 
 export const quickActionBonus = "2023-09-12";
+
+export const whatsappthanksUrl =
+  "https://chat.whatsapp.com/DgudhEKk2RCDK6WBKorwy1";
