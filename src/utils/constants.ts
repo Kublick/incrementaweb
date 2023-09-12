fffcc453e1bb30086bd2d0997c5bb74ce3e1555d
Max Blancarte <blancarte@gmail.com>
2023-09-12 12:52:23 -0700

    fix: enable all access

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,9 @@
 export const freeEventEndDate = "2023-10-02 11:00";
 
-export const allAccessEndDate = "2023-09-11 11:00";
+export const allAccessEndDate = "2023-09-14 23:00";
 
 export const salesEndDate = "2023-09-15 23:59";
 
 export const endEspecialEventDate = "2023-09-10";
 
 export const quickActionBonus = "2023-09-12 23:59";
-
-
