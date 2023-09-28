86494bd1d345aea9653215d6e63f010ec86b6b5b
Max Blancarte <blancarte@gmail.com>
2023-09-28 15:40:19 -0700

    fix: latam events

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
 export const freeEventEndDate = "2023-10-02 11:00";
 
-export const allAccessEndDate = "2023-09-14 23:00";
+export const allAccessEndDate = "2023-10-01 23:00";
 
-export const salesEndDate = "2023-09-15 23:59";
+export const salesEndDate = "2023-10-06 23:59";
 
-export const endEspecialEventDate = "2023-09-10";
+export const endEspecialEventDate = "2023-10-02";
 
-export const quickActionBonus = "2023-09-12 23:59";
+export const quickActionBonus = "2023-10-03 23:59";
