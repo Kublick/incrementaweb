bcd4830131dc514a42a844d11d24925703afa238
Max Blancarte <blancarte@gmail.com>
2023-10-12 08:12:36 -0700

    fix: update for november

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
-export const freeEventEndDate = "2023-10-02 11:00";
+export const freeEventEndDate = "2023-11-06 11:00";
 
-export const allAccessEndDate = "2023-10-01 23:00";
+export const allAccessEndDate = "2023-11-10 23:00";
 
-export const salesEndDate = "2023-10-06 23:59";
+export const salesEndDate = "2023-11-10 23:59";
 
-export const endEspecialEventDate = "2023-10-02";
+export const endEspecialEventDate = "2023-11-06";
 
-export const quickActionBonus = "2023-10-03 23:59";
+export const quickActionBonus = "2023-10-07 23:59";
