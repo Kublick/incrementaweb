683d8d90bb9d033af794987b14f03e8dc9e58dce
Max Blancarte <blancarte@gmail.com>
2025-02-25 15:19:25 -0800

    chore: update content

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
-export const freeEventEndDate = "2025-01-30 11:00";
+export const freeEventEndDate = "2025-03-27 11:00";
 
 export const allAccessEndDate = "2025-12-17 23:00";
 
-export const salesEndDate = "2025-02-03 11:00";
+export const salesEndDate = "2025-03-31 11:59";
 
 export const endEspecialEventDate = "2025-12-15";
 
