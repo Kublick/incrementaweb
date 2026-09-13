9991407fa231013f226a6aa2a7aa92ba4bb4e0e6
max blancarte <blancarte@gmail.com>
2026-09-12 21:16:53 -0700

    site update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
-export const freeEventEndDate = "2025-05-29 11:00";
+export const freeEventEndDate = "2026-08-10 11:00";
 
 export const allAccessEndDate = "2023-12-17 23:00";
 
-export const salesEndDate = "2025-06-02 11:00";
+export const salesEndDate = "2026-08-30 11:00";
 
 export const endEspecialEventDate = "2023-12-15";
 
